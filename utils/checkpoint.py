ddc9c60327dad554532ea323ca59457b279f68cd
seunghyeokleeme <seunghyeok.tech@gmail.com>
2025-03-16 18:49:09 +0900

    fix: RuntimeError: Attempting to deserialize object on a CUDA device but torch.cuda.is_available() is False. Use torch.load with map_location=torch.device('cpu')

--- utils/checkpoint.py
+++ utils/checkpoint.py
@@ -8,14 +8,14 @@ def save(ckpt_dir, net, optim, epoch):
     torch.save({'net': net.state_dict(), 'optim': optim.state_dict()},
                "%s/model_epoch%d.pth" % (ckpt_dir, epoch))
 
-def load(ckpt_dir, net, optim):
+def load(ckpt_dir, net, optim, device):
     if not os.path.exists(ckpt_dir):
         epoch  = 0
         return net, optim, epoch
     ckpt_list = os.listdir(ckpt_dir)
     ckpt_list.sort(key=lambda f: int(''.join(filter(str.isdigit, f))))
 
-    dict_model = torch.load('%s/%s' % (ckpt_dir, ckpt_list[-1]))
+    dict_model = torch.load('%s/%s' % (ckpt_dir, ckpt_list[-1]), map_location=device)
 
     net.load_state_dict(dict_model['net'])
     optim.load_state_dict(dict_model['optim'])
